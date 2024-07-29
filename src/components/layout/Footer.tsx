e41784aae9811f5aff73204ddad0a53b47098755
NTDung99 <dung.nguyen@thebank.vn>
2024-07-29 22:20:05 +0700

    fix: update text color in header and footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,9 @@
 const Footer = () => {
-    return (
-        <footer className="bg-secondary h-[106px] py-[26px] flex justify-center items-center">
-            <p className="mx-auto text-center">
-                Design by BinhLH
-            </p>
-        </footer>
-    )
-}
+  return (
+    <footer className="bg-secondary h-[106px] py-[26px] flex justify-center items-center">
+      <p className="mx-auto text-center text-white">Design by BinhLH</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
